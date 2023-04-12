b517a8870639a750e02b4d8ceb2f97a47c7b5702
Irving Alejandro Vega Lagunas <74569236+IrvingAlx@users.noreply.github.com>
2023-04-11 19:32:55 -0600

    Update of sui equation

--- src/com/iavl/modelo/Interim_Model.java
+++ src/com/iavl/modelo/Interim_Model.java
@@ -23,7 +23,7 @@ public class Interim_Model {
 		    this.Xh = -20*(Math.log10(Hm/2000));	    
 
 			for(int d=1; d < 20; d++) {
-		        LuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh + 10.6)*-1);
+		        LuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh + 10.6));
 			}
 
 		return LuArray;
@@ -37,7 +37,7 @@ public class Interim_Model {
 		    this.Xh = -20*(Math.log10(Hm/2000));
 		    
 			for(int d=1; d < 20; d++) {
-		        LsuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh + 9.6)*-1);
+		        LsuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh + 9.6));
 			}
 			
 		return LsuArray;
@@ -51,7 +51,7 @@ public class Interim_Model {
 	    this.Xh = -20*(Math.log10(Hm/2000));
 
 			for(int d=1; d < 20; d++) {
-		        LoArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh+8.2)*-1);
+		        LoArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh + 8.2));
 			}
 			
 	return LoArray;
@@ -65,7 +65,7 @@ public class Interim_Model {
 
 	    	for(int i =15; i < 40; i++) {
 				this.y = 4.6 + (0.0075*(i)) + (12.6/i); 
-	    		LuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh+10.6)*-1);
+	    		LuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh + 10.6)*-1);
 			}
 
 		return LuArray;
@@ -79,7 +79,7 @@ public class Interim_Model {
 		    
 	    	for(int i =15; i < 40; i++) {
 			    this.y = 4 + (0.0065*(i)) + (17.1/i);
-	    		LsuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh+9.6)*-1);
+	    		LsuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh - 9.6)*-1);
 			}
 			
 		return LsuArray;
@@ -93,7 +93,7 @@ public class Interim_Model {
 
 	    	for(int i =15; i < 40; i++) {
 	    	    this.y = 3.6 + (0.005*(i)) + (20/i);
-		        LoArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh+8.2)*-1);
+		        LoArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh + 8.2)*-1);
 			}
 			
 	return LoArray;
@@ -107,7 +107,7 @@ public class Interim_Model {
 	    	for(float i = (float)2.5; i < (float)2.7; i+=(float)0.01) {
 				this.A = 20*Math.log10(((4*Math.PI)*d0)/(299792458/i*1000000));
 			    this.Xf = 6*(Math.log10(i/2000));
-				LuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh+10.6)*-1);
+				LuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh + 10.6)*-1);
 			}
 
 		return LuArray;
@@ -121,7 +121,7 @@ public class Interim_Model {
 	    	for(float i = (float)2.5; i < (float)2.7; i+=(float)0.01) {
 			    this.A = 20*Math.log10(((4*Math.PI)*d0)/(299792458/i*1000000));
 			    this.Xf = 6*(Math.log10(i/2000));
-			    LsuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh+9.6)*-1);
+			    LsuArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh + 9.6)*-1);
 			}
 			
 		return LsuArray;
@@ -135,7 +135,7 @@ public class Interim_Model {
 			for(float i = (float)2.5; i < (float)2.7; i+=(float)0.01) {
 	    	    this.A = 20*Math.log10(((4*Math.PI)*d0)/(299792458/i*1000000));
 	    	    this.Xf = 6*(Math.log10(i/2000));
-		        LoArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh+8.2)*-1);
+		        LoArray.add((A + ((10*y)*(Math.log10(d/d0))) + Xf + Xh + 8.2)*-1);
 			}
 			
 	return LoArray;
